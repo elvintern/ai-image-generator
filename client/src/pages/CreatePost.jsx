aec0be761a2db132e95bc2ba3de1acf487d0a01c
elvintern <elvintern@gmail.com>
2023-02-21 17:48:40 +1300

    Add basic setup for express and mongodb

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import { preview } from '../assets';
 import { getRandomPrompt } from '../utils';
